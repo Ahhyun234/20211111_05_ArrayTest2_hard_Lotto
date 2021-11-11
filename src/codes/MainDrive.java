bffe925116db822fad913589b25bc47f5f00fddc
이아현 <suk03029@gamil.com>
2021-11-11 12:35:28 +0900

    Update MainDrive.java

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -11,8 +11,24 @@ public static void main(String[] args) {
 	int[] myInputNumbers = new int[6]; //입력한 숫자 6개 저장할 배열
 	
 	for(int i=0; i<myInputNumbers.length;i++) {
+		
+//		각 자리에 맞는 숫자를 입력할 때 올바를 숫자를 넣을 때 까지 입력하게 시킴
+		while(true) {
+			
+	
+		
 		System.out.println(i+1+"번째 숫자를 입력하세요");
 		int inputNum = myScanner.nextInt(); // 입력한 숫자를 scanner에 담음
+		
+		
+		//무조건 조건 통과
+		if(true) {
+			//써도 되는 숫자를 입력했다
+			myInputNumbers[i]=inputNum;
+			break;
+			
+		}
+	}	
 		
 	}
 	
