e3e77e603248727282bc9ae1a27580ca6d98c223
이아현 <suk03029@gamil.com>
2021-11-11 12:40:31 +0900

    숫자 범위 검사 진행
    1~45사이가 맞는지 질문 결과 저장
    결과 true맞는지

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -15,19 +15,26 @@ public static void main(String[] args) {
 //		각 자리에 맞는 숫자를 입력할 때 올바를 숫자를 넣을 때 까지 입력하게 시킴
 		while(true) {
 			
+
 	
 		
 		System.out.println(i+1+"번째 숫자를 입력하세요");
 		int inputNum = myScanner.nextInt(); // 입력한 숫자를 scanner에 담음
 		
+//		제약조건을 통과했는지 검사
+//		검사 1. 1~45 범위 맞는가? 결과를 boolean으로 저장
+		boolean isRangeOk = (1 <= inputNum) &&(InputNum<=45>);
+		
+		
 		
 		//무조건 조건 통과
-		if(true) {
+		if(isRangeOK) {
 			//써도 되는 숫자를 입력했다
 			myInputNumbers[i]=inputNum;
 			break;
 			
 		}
+		else {System.out.println("잘못된 숫자를 입력했습니다. 다시 입력해 주세요");
 	}	
 		
 	}
